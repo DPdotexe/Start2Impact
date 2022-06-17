72a45e9356d92560ffc774496dd39286f8e64174
DPdotexe <danielepuleri97@gmail.com>
2022-06-17 03:12:41 +0200

    Revert "Delete main.js"
    This reverts commit c9a861d4db7fa09eb7074369706645df0269bd9f.

---
+++ main.js
@@ -0,0 +1,13 @@
+
+function sendMail() {
+   let fullName = document.getElementById("name").value;
+   let userEmail = document.getElementById("email_id").value;
+   let userMessage = document.getElementById("message").value; 
+
+       var contactParams = {
+           from_name: fullName,
+           email_id: userEmail,
+           message: userMessage
+       }
+       emailjs.send('service_p1sl1w6', 'template_886zqlh', contactParams,'aZWFhGfDKVOa4iy5q').then(function (res) {})
+}
